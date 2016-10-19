f585b8beb123fc0113bf7686f197d12ec0ecf43c
JKSP <Biya@biya.com>
2016-10-19 17:57:21 -0400

    create function to convert list comma to array

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -3,8 +3,18 @@
 
 angular.module('LunchCheck',[])
 .controller('LunchCheckController',LunchCheckCtrlFunction);
-    
+
 LunchCheckCtrlFunction.$inject = ['$scope'];
+function LunchCheckCtrlFunction($scope){
+    var dishesArray =[];
+    $scope.myPlaceholder = 'list comma separated dishes you usually have for lunch';
+
+// function to convert string to Array
+    function listToArray(aList){
+        return aList.split(",");
+    };
+    console.log(listToArray("dede,frfrf,rrf,thyyhy"));
+};
 
 
 
