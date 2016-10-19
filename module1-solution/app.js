37787b5359f41f9d880aafceb77ee33491c2f63b
JKSP <Biya@biya.com>
2016-10-19 10:53:50 -0400

    Steps 7 to 10

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,6 +1,11 @@
 (function(){
 'use strict';
 
+angular.module('LunchCheck',[])
+.controller('LunchCheckController',LunchCheckCtrlFunction);
+    
+LunchCheckCtrlFunction.$inject = ['$scope'];
+
 
 
 })();
