dd8777291b06e809581f5a8aa16129a5034ca25d
Biya <joel.ksp@gmail.com>
2016-11-01 14:28:18 -0400

    Code getBoughtList, getToBuyList, and ng-repeat to display the lists in both controllers

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -1,4 +1,57 @@
 (function(){
 
-  angular.module('ShoppingListCheckOff',[]);
+  angular.module('ShoppingListCheckOff', [])
+    .controller('ToBuyController',ToBuyCtrlFunction)
+    .controller('AlreadyBoughtController',BoughtCtrlFunction)
+    .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+  
+  ToBuyCtrlFunction.$inject['ShoppingListCheckOffService'];
+  function ToBuyCtrlFunction(ShoppingListCheckOffService) {
+    var toBuyCtrl = this;
+    
+    // inialize the list to display
+    toBuyCtrl.toBuyList = ShoppingListCheckOffService.getToBuyList();
+   
+    toBuyCtrl.boughtButton = function () {
+    }
+
+  }
+
+  BoughtCtrlFunction.$inject['ShoppingListCheckOffService'];
+  function BoughtCtrlFunction(ShoppingListCheckOffService) {
+    var boughtCtrl = this;
+
+    boughtCtrl.boughtList = ShoppingListCheckOffService.getBoughtList();
+  }
+
+  function ShoppingListCheckOffService() {
+    var service = this;
+
+    // Initialize the list of items to buy    
+    var toBuyList = [{ name: "cookies", quantity: 10 },
+      { name: "pineapples", quantity: 2 },
+      { name: "apples", quantity: 8 },
+      { name: "potatoes", quantity: 6 },
+      { name: "bottle of milk", quantity: 3 }];
+    
+    // Initialize the list of already bought items
+    var boughtList = [];
+
+    service.getToBuyList = function () {
+      console.log("Enter getToBuyList()");
+      return toBuyList;
+
+    }
+    service.getBoughtList = function () {
+      console.log("Enter getBoughtList");
+      return boughtList;
+    }
+    service.boughtButton = function (toBuyList,boughtList) {
+      
+    }
+
+  }
+  
+
+
 })();
