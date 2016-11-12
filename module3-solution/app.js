f7c188bd6973a857fc8f9653460fa09fc6e7b671
Biya <joel.ksp@gmail.com>
2016-11-12 09:01:50 -0400

    Implement process Items function

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -5,11 +5,65 @@
         .controller('NarrowItDownController', nidCtrlFunction)
         .service('MenuSearchService', MenuSearchService);
     
-    MenuSearchService.$inject['$http']
+    nidCtrlFunction.$inject = ['MenuSearchService'];
+    function nidCtrlFunction(MenuSearchService) {
+        var nidCtrl = this;
+        console.log('CONTROLLER');
+        nidCtrl.getFoundItems = function () {
+            console.log('Entree dans fonction clic');
+            var promise = MenuSearchService.getMatchedMenuItems(nidCtrl.searchWord);       
+            promise.then(function (response) {
+                nidCtrl.found = response;
+                console.log('found: ', nidCtrl.found);
+            });
+
+            // console.log('typeOf nidCtrl.found: ', typeof (nidCtrl.found));
+            // console.log('nidCtrl.found: ', nidCtrl.found);
+
+    
+        }; //getFoundItems
+
+    }; //nidCtrlFunction
+
+
+    MenuSearchService.$inject = ['$http'];
     function MenuSearchService($http) {
         var service = this;
 
         service.getMatchedMenuItems = function (searchTerm) {
+
+            if (searchTerm == "") {
+                return [];
+            } else {
+                return $http({url:'https://davids-restaurant.herokuapp.com/menu_items.json'})
+                    .then(function (result) {
+                    
+                    // process result and only keep items that match
+                    var foundItems = [];
+                    var processedItems = result.data.menu_items;
+                    console.log('items.length: ', processedItems.length);
+                    console.log('************');
+
+                    var cpt = 0;
+                    for (var i = 0; i < processedItems.length; i++){
+                        var processedString = processedItems[i].description;
+                        if (processedString.indexOf(searchTerm) != -1) {
+                            
+                            foundItems.push(processedItems[i]);
+                            console.log('foundItems[' + cpt + ']: ', foundItems[cpt].description);
+                            cpt++;
+                        }
+
+                    }; // for
+                    console.log('foundItems.length: ', foundItems.length);
+                    console.log('foundItems: ', foundItems);
+
+                    // return processed items
+                    return foundItems;
+                        
+                    }); // end of then
+
+            }
             
         };
     };
